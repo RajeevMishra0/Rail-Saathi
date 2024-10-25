b6fe26207a8f68522a01f70f250fe9f759ff649d
AyushGoel0 <aayushgoel524@gmail.com>
2024-10-25 09:24:39 +0530

    New file-3

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 from app.app import app, db
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 # from flask_script import Manager
 
 migrate = Migrate(app, db)
+
+# Add migrate command to the manager
+app.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source .venv/Scripts/activate
+pip install -r requirements.txt
 gunicorn app.app:app
