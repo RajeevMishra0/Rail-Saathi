41981369f60bb72eee1c71067d06a84a25850d44
Rajeev Mishra <146163242+RajeevMishra0@users.noreply.github.com>
2024-11-19 09:58:22 +0530

    UPDATE-8

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Rajeev Mishra\OneDrive\Desktop\edit rs personal\.venv
+set VIRTUAL_ENV=C:\Users\Rajeev Mishra\OneDrive\Desktop\Rail-Saathi\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
